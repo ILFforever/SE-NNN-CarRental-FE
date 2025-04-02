13cd624a2d782741a089a1c77a544fb9eeb0d6f7
ILFforever <hammymukura@gmail.com>
2025-04-02 11:08:29 +0700

    Try pushing Again

--- SetupMultiRepo.bat
+++ SetupMultiRepo.bat
@@ -3,7 +3,7 @@ echo Setting up Git remotes...
 
 git remote add all https://github.com/ILFforever/SE-NNN-CarRental-FE
 git remote set-url --add --push all https://github.com/ILFforever/SE-NNN-CarRental-FE
-git remote set-url --add --push all https://github.com/2110503-2564/se-project-21_nnn-community/tree/main
+git remote set-url --add --push all https://github.com/2110503-2564/se-project-21_nnn-community/
 git branch -u origin/main
 git remote -v
 
