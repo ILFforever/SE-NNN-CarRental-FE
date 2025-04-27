d34a2a554d0c786385788cd01c802b33223bc010
Pisit Kanchanapinpong <pisit.eicq@gmail.com>
2025-04-27 18:52:56 +0700

    update testcase2

--- tests/us2-4_discount.spec.ts
+++ tests/us2-4_discount.spec.ts
@@ -9,7 +9,7 @@ test('should see discount on summary', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).fill('12345678');
   await page.getByRole('button', { name: 'Sign In as User' }).click();
   await page.getByRole('link', { name: 'Catalog' }).click();
-  await page.locator('[data-test-id="catalog"] div').filter({ hasText: '$2.00/dayconvertiblePlaywright Hand2025 • Playwright TestRated by 13 people5.0$' }).getByRole('button').nth(3).click();
+  await page.locator('[data-test-id="catalog"] div').filter({ hasText: '$2.00/dayconvertiblePlaywright Hand2025 • Playwright TestRated by 0 people0.0$2' }).getByRole('button').nth(3).click();
   await page.locator('div').filter({ hasText: /^Pickup Date$/ }).getByRole('textbox').fill('2025-05-05');
   await page.locator('div').filter({ hasText: /^Return Date$/ }).getByRole('textbox').fill('2025-05-09');
   await expect(page.getByRole('main')).toContainText('$8.50');
@@ -25,9 +25,8 @@ test('should not see discount on summary', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).fill('12345678');
   await page.getByRole('button', { name: 'Sign In as User' }).click();
   await page.getByRole('link', { name: 'Catalog' }).click();
-  await page.locator('[data-test-id="catalog"] div').filter({ hasText: '$2.00/dayconvertiblePlaywright Hand2025 • Playwright TestRated by 13 people5.0$' }).getByRole('button').nth(3).click();
+  await page.locator('[data-test-id="catalog"] div').filter({ hasText: '$2.00/dayconvertiblePlaywright Hand2025 • Playwright TestRated by 0 people0.0$2' }).getByRole('button').nth(3).click();
   await page.locator('div').filter({ hasText: /^Pickup Date$/ }).getByRole('textbox').fill('2025-05-05');
   await page.locator('div').filter({ hasText: /^Return Date$/ }).getByRole('textbox').fill('2025-05-09');
-  await expect(page.getByRole('main')).not.toContainText('$8.50');
-  await expect(page.getByRole('main')).not.toContainText('Loyalty Discount (15%):');
+  await expect(page.getByRole('main')).toContainText('$10.00');
 });
