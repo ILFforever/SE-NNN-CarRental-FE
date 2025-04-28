7438c6018e39ab0004c13d3afa83e9d4060eabe2
PUN <tmr.parintorn@gmail.com>
2025-04-28 20:58:21 +0700

    test2-2 fix

--- tests/us2-2_Renter_edit_booking.spec.ts
+++ tests/us2-2_Renter_edit_booking.spec.ts
@@ -14,15 +14,16 @@ test("User want to change valid date", async ({ page }) => {
     await page.getByRole('button', { name: /View Car/i }).first().click();
     await page.locator('div').filter({ hasText: /^Pickup Date$/ }).getByRole('textbox').fill('2026-09-09');
     await page.getByRole('button', { name: /^Confirm & Pay/i }).click();
-   // await page.getByLabel('Close').click();
+    await page.getByLabel('Close').click();
     //edit reservation
     await page.getByRole("button", { name: "Playwright Man" }).click();
     await page.getByRole("link", { name: "My Reservations" }).click();
     await page.locator('.MuiBackdrop-root').click();
     await page.locator('button').filter({ hasText: /^Edit Reservation/i }).first().click();
-    await page.locator('div').filter({ hasText: /^Start Date$/ }).getByRole('textbox').fill('2027-09-09');
+    await page.locator('div').filter({ hasText: /^Start Date$/ }).getByRole('textbox').fill('2026-08-09');
+    await page.locator('div').filter({ hasText: /^Return Date$/ }).getByRole('textbox').fill('2026-08-10');
     await page.getByRole('button', { name: /^Save/i }).click();
-    await expect(page.getByText('Reservation updated successfully')).toBeVisible({timeout:100000});
+    await expect(page.locator('div.mb-6').getByText('Reservation updated successfully').first()).toBeVisible({ timeout: 100000 });
 });
 
 test("User want to change invalid date", async ({ page }) => {
@@ -39,7 +40,7 @@ test("User want to change invalid date", async ({ page }) => {
     await page.getByRole('button', { name: /View Car/i }).first().click();
     await page.locator('div').filter({ hasText: /^Pickup Date$/ }).getByRole('textbox').fill('2026-09-09');
     await page.getByRole('button', { name: /^Confirm & Pay/i }).click();
-    //await page.getByLabel('Close').click();
+    await page.getByLabel('Close').click();
     //edit reservation
     await page.getByRole("button", { name: "Playwright Man" }).click();
     await page.getByRole("link", { name: "My Reservations" }).click();
