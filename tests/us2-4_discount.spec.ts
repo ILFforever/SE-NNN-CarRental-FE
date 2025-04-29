d6df1993aa0a1dd62e9d2e26c1f3f564a7378791
Pisit Kanchanapinpong <pisit.eicq@gmail.com>
2025-04-29 21:52:22 +0700

    fix test2-4

--- tests/us2-4_discount.spec.ts
+++ tests/us2-4_discount.spec.ts
@@ -8,8 +8,9 @@ test('should see discount on summary', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('12345678');
   await page.getByRole('button', { name: 'Sign In as User' }).click();
+  await page.waitForTimeout(3000);
   await page.getByRole('link', { name: 'Catalog' }).click();
-  await page.locator('[data-test-id="catalog"] div').filter({ hasText: '$2.00/dayconvertiblePlaywright Hand2025 • Playwright TestRated by 0 people0.0$2' }).getByRole('button').nth(3).click();
+  await page.locator('[data-test-id="catalog"] div').filter({ hasText: '$2.00/daysedanDiscount 2-' }).getByRole('button').nth(1).click();
   await page.locator('div').filter({ hasText: /^Pickup Date$/ }).getByRole('textbox').fill('2025-05-05');
   await page.locator('div').filter({ hasText: /^Return Date$/ }).getByRole('textbox').fill('2025-05-09');
   await expect(page.getByRole('main')).toContainText('$8.50');
@@ -24,8 +25,9 @@ test('should not see discount on summary', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('12345678');
   await page.getByRole('button', { name: 'Sign In as User' }).click();
+  await page.waitForTimeout(3000);
   await page.getByRole('link', { name: 'Catalog' }).click();
-  await page.locator('[data-test-id="catalog"] div').filter({ hasText: '$2.00/dayconvertiblePlaywright Hand2025 • Playwright TestRated by 0 people0.0$2' }).getByRole('button').nth(3).click();
+  await page.locator('[data-test-id="catalog"] div').filter({ hasText: '$2.00/daysedanDiscount 2-' }).getByRole('button').nth(1).click();
   await page.locator('div').filter({ hasText: /^Pickup Date$/ }).getByRole('textbox').fill('2025-05-05');
   await page.locator('div').filter({ hasText: /^Return Date$/ }).getByRole('textbox').fill('2025-05-09');
   await expect(page.getByRole('main')).toContainText('$10.00');
