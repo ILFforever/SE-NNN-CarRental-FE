6cac0daf24f4e34951698cc94fcc1b3a5f43f3dc
Watcharawit Pratpiamsuk <sayhi7562@gmail.com>
2025-04-29 21:48:45 +0700

    fix us1-3

--- tests/us1-3_ratingandreview.spec.ts
+++ tests/us1-3_ratingandreview.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 
 test('car details page shows rating of car provider', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('http://se-cedt-rentals.vercel.app/');
     await page.getByRole('link', { name: 'Sign-In' }).click();
     await page.getByRole('textbox', { name: 'Email Address' }).click();
     await page.getByRole('textbox', { name: 'Email Address' }).fill('playwright@gmail.com');
@@ -18,7 +18,7 @@ test('car details page shows rating of car provider', async ({ page }) => {
 
   test('user can review after status is Completed', async ({ page }) => {
     // First, we need to sign in as a user and make a reservation
-    await page.goto('http://localhost:3000/');
+    await page.goto('http://se-cedt-rentals.vercel.app/');
     await page.getByRole('link', { name: 'Sign-In' }).click();
     await page.getByRole('textbox', { name: 'Email Address' }).click();
     await page.getByRole('textbox', { name: 'Email Address' }).fill('playwright@gmail.com');
@@ -32,8 +32,9 @@ test('car details page shows rating of car provider', async ({ page }) => {
     await page.getByRole('button', { name: /^Confirm & Pay/i }).click();
     await page.getByLabel('Close').click();
     await page.getByRole('button', { name: 'Playwright Man' }).click();
-    await page.getByRole('link', { name: 'Sign Out' }).click();
+    await page.getByRole('button', { name: 'Sign Out' }).click();
     await page.getByRole('button', { name: 'Yes, Sign Out' }).click();
+    await page.waitForTimeout(3000);
     // Now, we can sign in as the admin and mark the reservation as unpaid
     await page.getByRole('link', { name: 'Sign-In' }).click();
     await page.getByRole('textbox', { name: 'Email Address' }).click();
@@ -43,16 +44,16 @@ test('car details page shows rating of car provider', async ({ page }) => {
     await page.getByRole('button', { name: 'Sign In as User' }).click();
     await page.waitForTimeout(3000);
     await page.getByRole('button', { name: 'Playwright Admin' }).click();
-    await page.getByRole('link', { name: 'Admin Tools' }).click();
-    await page.locator('.MuiBackdrop-root').click();
+    await page.getByRole('button', { name: 'Admin Tools' }).click();
     await page.getByRole('link', { name: 'View All Bookings' }).click();
     await page.getByRole('button', { name: 'Confirm reservation' }).first().click();
     await page.getByRole('button', { name: 'Yes, Confirm Reservation' }).click();
     await page.getByRole('button', { name: 'Mark as unpaid' }).click();
     await page.getByRole('button', { name: 'Yes, Mark as Unpaid' }).click();
     await page.getByRole('button', { name: 'Playwright Admin' }).click();
-    await page.getByRole('link', { name: 'Sign Out' }).click();
+    await page.getByRole('button', { name: 'Sign Out' }).click();
     await page.getByRole('button', { name: 'Yes, Sign Out' }).click();
+    await page.waitForTimeout(3000);
     // Now, we can sign in as the user and pay for the reservation and review the reservation
     await page.getByRole('link', { name: 'Sign-In' }).click();
     await page.getByRole('textbox', { name: 'Email Address' }).click();
@@ -62,9 +63,8 @@ test('car details page shows rating of car provider', async ({ page }) => {
     await page.getByRole('button', { name: 'Sign In as User' }).click();
     await page.waitForTimeout(3000);
     await page.getByRole('button', { name: 'Playwright Man' }).click();
-    await page.getByRole('link', { name: 'My Reservations' }).click();
-    await page.locator('.MuiBackdrop-root').click();
-    await page.getByRole('button', { name: 'Pay Now' }).first().click();
+    await page.getByRole('button', { name: 'My Reservations' }).click();
+    await page.getByRole('button', { name: 'Pay Now' }).click();
     await page.getByRole('button', { name: 'Pay $' }).click();
     await page.getByRole('link', { name: 'View My Reservations' }).click();
     await page.getByRole('button', { name: 'Review Provider' }).first().click();
@@ -73,8 +73,9 @@ test('car details page shows rating of car provider', async ({ page }) => {
     await page.getByRole('button', { name: 'Confirm Rating' }).click();
     await expect(page.getByText('Already Reviewed').first()).toBeVisible();
     await page.getByRole('button', { name: 'Playwright Man' }).click();
-    await page.getByRole('link', { name: 'Sign Out' }).click();
+    await page.getByRole('button', { name: 'Sign Out' }).click();
     await page.getByRole('button', { name: 'Yes, Sign Out' }).click();
+    await page.waitForTimeout(1000);
   });
 
 
